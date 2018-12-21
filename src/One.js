e15129c52af6674a9540d5e11c545ada36cfa3f0
santosh <santosh.ece06@gmail.com>
2018-12-21 12:57:03 +0530

    approach 1

--- src/One.js
+++ src/One.js
@@ -1,6 +1,16 @@
+const find = require('local-devices');
 
 module.exports = {
     "exec" : ()=>{
         console.log("ONE - 1111");
+
+        let mpromise = find();
+        mpromise.then(devices => {
+            console.log(devices);
+        }).catch((err)=>{
+            console.log(err);
+        });
     }
 }
+
+
